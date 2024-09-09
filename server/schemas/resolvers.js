6f6411d948f2b3334fc1193eb5b22030d6f79fd7
akaufmanfrey <akaufmanfrey@gmail.com>
2024-09-09 17:24:50 -0400

    added backend graphql

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,62 @@
+const { saveBook } = require('../controllers/user-controller');
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw AuthenticationError;
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, username, password }) => {
+      const user = await User.findOne({ $or: [{ username: username }, { email: email }] });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create(
+        {
+          username,
+          email,
+          password,
+        },
+      );
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { book }, context) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { savedBooks: book } },
+        { new: true, runValidators: true }
+      );
+      return updatedUser
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId: bookId } } },
+        { new: true }
+      );
+      return updatedUser
+    },
+  }
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const typeDefs = `
+  input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: String!
+    user: User!
+  }
+  type Query {
+    me: [User]
+  }
+
+  type Mutation {
+    saveBook(book: BookInput!): User
+    createVote(_id: String!, techNum: Int!): Matchup
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
